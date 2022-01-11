a348321b996774c960ce94e352943c594e7de94c
Anastasiya-Ko <91557834+Anastasiya-Ko@users.noreply.github.com>
2022-01-11 17:12:11 +0500

    Delete pet_reestr directory

--- pet_reestr/src/main/java/com/example/pet/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.pet.exception;
-
-public class ResourceNotFoundException extends Exception{
-    private static final long serialVersionUID = 1L;
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
