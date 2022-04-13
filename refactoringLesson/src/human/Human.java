18bed337b779cd59b3cfc6c9bcc3cb16a9845dbd
Anastasiya-Ko <91557834+Anastasiya-Ko@users.noreply.github.com>
2022-04-13 14:52:31 +0500

    Задача на рефакторинг

---
+++ refactoringLesson/src/human/Human.java
@@ -0,0 +1,84 @@
+package human;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Human implements Alive{
+
+    public class Size{
+        public int height;
+        public int weight;
+    }
+
+    private static int nextId = 0;
+    private int id;
+    protected int age;
+    protected String name;
+    protected Size size;
+    private BloodGroup bloodGroup;
+    private List<Human> children = new ArrayList<>();
+
+
+    public Human(String name, int age) {
+        this.name=name;
+        this.age=age;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    // метод гетЧилдрен возвращает немодифицируемое представление списка чайлд
+    public List<Human> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void live(){}
+    public int getId() {
+        return id;
+    }
+
+    public void printSize() {
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
+    }
+
+    public void addChild(Human child){
+        children.add(child);
+    }
+    public  void removeChild(Human child){
+        children.remove(child);
+    }
+
+    public String getPosition(){
+        return "Человек";
+    }
+
+    public void printData() {
+        System.out.println(getPosition() + name);
+    }
+
+
+}
